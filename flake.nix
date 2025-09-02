9d2b535ced3fe523174fa716a3adc10321ec39d6
Clément Songis <5clemans585@gmail.com>
2025-09-02 22:07:13 +0200

    fix: remove FUSE-dependent tests

--- flake.nix
+++ flake.nix
@@ -34,6 +34,15 @@
 
           vendorHash = "sha256-r8zE1mwoPOcsMeUsm1Rfq5XQ6Rze/gmcMwilzfM4xOk=";
 
+          checkPhase = ''
+            runHook preCheck
+            # Exclude the FUSE-dependent package from tests
+            for pkg in $(go list ./... | grep -v 'cmd/plakar/subcommands/mount'); do
+              go test -vet=off "$pkg"
+            done
+            runHook postCheck
+          '';
+
           meta = with pkgs.lib; {
             homepage = "https://plakar.io";
             changelog = "https://github.com/PlakarKorp/plakar/releases/tag/v${version}";
